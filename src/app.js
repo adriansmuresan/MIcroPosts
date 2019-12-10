ea128668b406e24e01f38edd63f3f623fa93efb3
Adrian S MUresan <adriansmuresan@gmail.com>
2019-12-09 19:15:10 -0600

    Adds event listener for edit and declares some variables for the edit function

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 // Listen for delete
 document.querySelector('#posts').addEventListener('click', deletePost);
 
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
 function getPosts() {
   http.get('http://localhost:3000/posts')
   .then(data => ui.showPosts(data))
@@ -37,7 +40,6 @@ function submitPost() {
 
 // Delete Post
 function deletePost(e) {
-  e.preventDefault();
   if(e.target.parentElement.classList.contains('delete')) {
     const id = e.target.parentElement.dataset.id;
     if(confirm('Are you sure?')) {
@@ -49,4 +51,24 @@ function deletePost(e) {
         .catch(err => console.log(err));
     }
   }
+  e.preventDefault();
+}
+
+// Enable Edit State
+function enableEdit(e) {
+  if(e.target.parentElement.classList.contains('edit')){
+    const id = e.target.parentElement.dataset.id;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Fill form with current post
+    ui.fillForm(data);
+  }
+  e.preventDefault();
 }
